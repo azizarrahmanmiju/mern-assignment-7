348f328847dbf10f09cb2f0b3b281408f396342c
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-12 21:52:35 +0600

    Header

---
+++ src/Header/Header.jsx
@@ -0,0 +1,26 @@
+
+const Header = () => {
+    return (
+        <div className="" >
+
+            <div className="flex headerdiv justify-between mx-[100px]  items-center my-8">
+                <h1 className="text-4xl font-bold">Recipe Calories</h1>
+                <ul className="list">
+                    <li><a>Home</a></li>
+                    <li><a>Recipie</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Search</a></li>
+                </ul>
+                <div className="flex justify-center content-center gap-2">
+
+                    <input className="border-2 p-2 rounded-full" type="text" placeholder='Search'></input>
+
+                    <img className="border-2 w-10 h-10 rounded-full border-red-300" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" alt="" />
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
